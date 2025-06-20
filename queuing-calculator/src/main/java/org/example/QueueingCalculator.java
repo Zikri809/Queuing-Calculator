f2f57dc1e364cad5de444c1d09ced1d685dd4b8c
Zikri809 <mohdzikri809@gmail.com>
2025-06-20 10:26:17 +0800

    Basic interface completed

--- queuing-calculator/src/main/java/org/example/QueueingCalculator.java
+++ queuing-calculator/src/main/java/org/example/QueueingCalculator.java
@@ -1,16 +1,26 @@
 package org.example;
 
+
 import java.util.Scanner;
-class  MM1{
+abstract class Queue{
     double lambda;
     double mu;
-    int c = 1;
+    int c ;
     double rho;
     double L  ;
     double Lq ;
     double W  ;
     double Wq ;
+    double [] Pn = new double [6];
+    String system_type;
+
+    public abstract void Calculate();
+}
+class  MM1 extends Queue{
+
     public MM1(){
+        this.c = 1;
+        this.system_type = "MM1";
         Scanner scanner = new Scanner(System.in);
 
         do{
@@ -46,39 +56,36 @@ class  MM1{
         this.Lq= this.lambda * this.lambda / (this.mu * (this.mu - this.lambda));
         this.W= 1 / (this.mu - this.lambda);
         this.Wq= this.lambda / (this.mu * (this.mu - this.lambda));
+        for (int n = 0; n <= 5; n++) {
+            this.Pn[n] = (1 - this.rho) * Math.pow(this.rho, n);
+        }
+
+    }
+    public void Display(){
+        Calculate();
         System.out.printf("M/M/1 Results:%n");
-        System.out.printf("Traffic Intensity (rho): %.4f%n”, rho");
+        System.out.printf("Traffic Intensity (rho): %.4f%n", this.rho);
         System.out.printf("Expected Number in System (L): %.4f%n", this.L);
         System.out.printf("Expected Queue Length (Lq): %.4f%n", this.Lq);
         System.out.printf("Expected Time in System (W): %.4f hours%n", this.W);
         System.out.printf("Expected Waiting Time in Queue (Wq): %.4f hours%n", this.Wq);
+
+    }
+    public void Display_probability(){
+        Calculate();
         for (int n = 0; n <= 5; n++) {
-            double Pn = (1 - this.rho) * Math.pow(this.rho, n);
-            System.out.printf("P(%d customers): %.4f%n", n, Pn);
+            System.out.printf("P(%d customers): %.4f%n", n, this.Pn[n]);
         }
     }
 }
 
 
-
-
-
-
-
-
-class MMC{
-    double lambda;
-    double mu;
-    int c;
-    double rho;
-    double L  ;
-    double Lq ;
-    double W  ;
-    double Wq ;
+class MMC extends Queue{
     double P0;
-    double Pn;
+
     public MMC(){
         Scanner scanner = new Scanner(System.in);
+
 
         do{
             System.out.print("Enter arrival rate (lambda): ");
@@ -111,6 +118,7 @@ class MMC{
                 System.out.print("Please reenter all the necessary parameters!");
             }
         }while(this.lambda / (this.c * this.mu) >= 1);
+        this.system_type = "MM"+this.c;
 
     }
     public void Calculate(){
@@ -126,15 +134,25 @@ class MMC{
         this.L = this.Lq + this.c * this.rho;
         this.Wq = Lq / lambda;
         this.W = this.Wq + 1 / this.mu;
+        for (int n = 0; n <= 5; n++) {
+            this.Pn[n] = (n < this.c) ? this.P0 * Math.pow(this.c * this.rho, n) / factorial(n) : this.P0 * Math.pow(this.c * this.rho, n) / (Math.pow(this.c, n -this.c) * factorial(this.c));
+        }
+
+    }
+    public void Display(){
+        Calculate();
         System.out.printf("M/M/%d Results:%n", this.c);
         System.out.printf("Traffic Intensity (rho): %.4f%n", this.rho);
         System.out.printf("Expected Number in System (L): %.4f%n", this.L);
         System.out.printf("Expected Queue Length (Lq): %.4f%n", this.Lq);
         System.out.printf("Expected Time in System (W): %.4f hours%n ", this.W);
         System.out.printf("Expected Waiting Time in Queue (Wq): %.4f hours%n", this.Wq);
+
+    }
+    public void Display_probability(){
+        Calculate();
         for (int n = 0; n <= 5; n++) {
-            this.Pn = (n < this.c) ? this.P0 * Math.pow(this.c * this.rho, n) / factorial(n) : this.P0 * Math.pow(this.c * this.rho, n) / (Math.pow(this.c, n -this.c) * factorial(this.c));
-            System.out.printf("P(%d customers): %.4f%n", n, this.Pn);
+            System.out.printf("P(%d customers): %.4f%n", n, this.Pn[n]);
         }
     }
     public long factorial(int n) {
@@ -181,39 +199,60 @@ public static void main(String[] args) {
                 queue_system = scanner.nextInt();
 
             }
+            System.out.println("=======================================================================================");
             if(queue_system == 1){
-                System.out.println("=======================================================================================");
                 final MM1 MM1_object = new MM1();
-                MM1_object.Calculate();
                 System.out.println("=======================================================================================");
+                MM1_object.Display();
             }
             else{
-                System.out.println("=======================================================================================");
                 final MMC MMC_object = new MMC();
-                MMC_object.Calculate();
                 System.out.println("=======================================================================================");
+                MMC_object.Display();
             }
+
         }
         else{
-            System.out.println("1 - MM1 (Single server system) \n2 - MMC (Multiple server system) ");
-            System.out.print("Enter desired system: ");
-            int queue_system = scanner.nextInt();
-            while(queue_system<1 || queue_system>2){
-                System.out.printf("Invalid Input! Please enter again.%n");
+            Queue [] arry = new Queue[2];
+            for(int i = 0; i < 2; i++){
+                System.out.println("=======================================================================================");
                 System.out.println("1 - MM1 (Single server system) \n2 - MMC (Multiple server system) ");
                 System.out.print("Enter desired system: ");
-                queue_system = scanner.nextInt();
+                int queue_system = scanner.nextInt();
+                while(queue_system<1 || queue_system>2){
+                    System.out.printf("Invalid Input! Please enter again.%n");
+                    System.out.println("1 - MM1 (Single server system) \n2 - MMC (Multiple server system) ");
+                    System.out.print("Enter desired system: ");
+                    queue_system = scanner.nextInt();
 
+                }
+                if(queue_system == 1){
+                    arry[i] = new MM1();
+                    arry[i].Calculate();
+                }
+                else{
+                    arry[i] = new MMC();
+                    arry[i].Calculate();
+                }
             }
-            if(queue_system == 1){
-                final MM1 MM1_object = new MM1();
-                MM1_object.Calculate();
+            System.out.println("=======================================================================================");
+            System.out.printf("Comparison Calculation Mode:%n");
+            System.out.printf("%50s%10s%10s%n","",arry[0].system_type,arry[1].system_type);
+            System.out.printf("%50s%10.4f%10.4f%n","Traffic Intensity (rho): ",arry[0].rho,arry[1].rho);
+            System.out.printf("%50s%10.4f%10.4f%n","Expected Number in System (L): ",arry[0].L,arry[1].L);
+            System.out.printf("%50s%10.4f%10.4f%n","Expected Queue Length (Lq): ",arry[0].Lq,arry[1].Lq);
+            System.out.printf("%50s%10.4f%10.4f%n","Expected Time in System in hours (W): ",arry[0].W,arry[1].W);
+            System.out.printf("%50s%10.4f%10.4f%n","Expected Waiting Time in Queue in hours(Wq): ",arry[0].Wq,arry[1].Wq);
+            for(int i = 0; i < 6; i++){
+                System.out.printf("%34sP(%d customers): %10.4f%10.4f%n","",i,arry[0].Pn[i],arry[1].Pn[i]);
             }
-            else{
-                final MMC MMC_object = new MMC();
-                MMC_object.Calculate();
-            }
+
+
+
         }
+        System.out.println("=======================================================================================");
+        System.out.print("Enter [0-to stop enter 1-to continue] :");
+        proceed = scanner.nextInt();
 
 
     }
